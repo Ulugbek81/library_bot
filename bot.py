0abe48e2e3518ab1acd47daf61e537b83922fc87
Ulan <42806502+Kospanulan@users.noreply.github.com>
2020-03-21 17:04:37 +0600

    Update bot.py

--- bot.py
+++ bot.py
@@ -129,5 +129,5 @@ def check_error_msg(message):
 
 if __name__=='__main__':
 	server.debug = True
-	server.run(host="0.0.0.0", port=int(os.environ.get('PORT', 5000)))
+	server.run(host="0.0.0.0", port=int(os.environ.get('PORT', 8000)))
 	#bot.polling(none_stop=True)
